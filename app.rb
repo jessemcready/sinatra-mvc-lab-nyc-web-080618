03103e4e72a1e514378b94864e57d7e84be453c9
Jesse McReady <jessemcready@gmail.com>
2018-08-27 17:34:27 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    pig_latin = PigLatinizer.new
+    @new_string = pig_latin.piglatinize(params[:user_phrase])
+    erb :piglatinize_phrase
+  end
+
 end
